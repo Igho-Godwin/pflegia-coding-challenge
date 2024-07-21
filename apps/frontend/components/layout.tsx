758dfe5289f11dc9a9395f3d2d12fd9287182e2e
Avwerosuo <ighogodwinavweroswo@gmail.com>
2024-07-21 16:24:11 +0200

    destructured slug and stored it in a variable, make sure if you click on back to the index page the difficulty and title goes out

--- apps/frontend/components/layout.tsx
+++ apps/frontend/components/layout.tsx
@@ -11,12 +11,12 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const router = useRouter();
-
+  const { slug } = router.query 
   const { setData } = usePizzaContext();
-  const url = `${process.env.NEXT_PUBLIC_API_URL}${router.query.slug}`;
+  const url = `${process.env.NEXT_PUBLIC_API_URL}${slug}`;
 
   const { data: pizza } = useFetch<Pizza>(url, {
-    enabled: Boolean(router.query.slug),
+    enabled: Boolean(slug),
   });
 
   useEffect(() => {
@@ -50,8 +50,8 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
         <header className="flex flex-col sticky top-0 z-50 bg-white">
           <h2 className="text-gray-700 text-6xl font-semibold">Pizzaria</h2>
           <h3 className="text-2xl font-semibold">
-            {pizza?.name ? pizza.name : 'How to make your Pizza with love:'}
-            {badgeColor && (
+            {pizza?.name && slug ? pizza.name : 'How to make your Pizza with love:'}
+            {badgeColor && slug && (
               <span
                 className={`text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded  ml-5 ${badgeColor}`}
               >
