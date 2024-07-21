c0c698a6f939cdf56b0a0d1e758663fcdc855a6c
Avwerosuo <ighogodwinavweroswo@gmail.com>
2024-07-21 16:37:59 +0200

    formateed MetaDataType file

--- apps/frontend/shared/types/MetaDataType.ts
+++ apps/frontend/shared/types/MetaDataType.ts
@@ -1,11 +1,10 @@
-
 type MetaDataType = {
-    page: number;
-    take: number;
-    itemCount: number;
-    pageCount: number;
-    hasPreviousPage: boolean;
-    hasNextPage: boolean
-}
+  page: number;
+  take: number;
+  itemCount: number;
+  pageCount: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+};
 
 export default MetaDataType;
