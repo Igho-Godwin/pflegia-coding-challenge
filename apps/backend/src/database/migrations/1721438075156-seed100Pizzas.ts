e99afb0b6b39e38d330ba50b976353185825a2da
Avwerosuo <ighogodwinavweroswo@gmail.com>
2024-07-21 18:37:42 +0200

    updated seeder to add 100 pizzas with more dynamic data from faker

--- apps/backend/src/database/migrations/1721438075156-seed100Pizzas.ts
+++ apps/backend/src/database/migrations/1721438075156-seed100Pizzas.ts
@@ -1,28 +1,21 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { faker } from '@faker-js/faker';
+import { times } from 'lodash';
 
 export class Seed100Pizzas1721438075156 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     for (let i = 1; i <= 100; i++) {
-      const name = faker.person.firstName() + ' pizza';
+      const name = faker.commerce.product() + ' pizza';
       const rating = faker.number.int({ min: 1, max: 12 });
-      const imageUrl = faker.image.urlLoremFlickr({ category: 'food' });
-      const ingredients = [
-        'Pizza dough',
-        'Tomato sauce',
-        'Fresh mozzarella cheese',
-        'Fresh basil leaves',
-        'Olive oil',
-        'Salt',
-      ];
-      const instructions = [
-        'Preheat the oven to 475°F (245°C).',
-        'Roll out the pizza dough on a floured surface.',
-        'Spread tomato sauce evenly on the dough.',
-        'Add slices of fresh mozzarella cheese on top.',
-        'Bake in the oven for 10-12 minutes until the crust is golden brown and the cheese is bubbly.',
-        'Garnish with fresh basil leaves and a drizzle of olive oil before serving.',
-      ];
+      const imageUrl = faker.image.urlLoremFlickr({
+        category: 'food'
+      });
+      const ingredients = times(faker.number.int({ min: 1, max: 7 }), () =>
+        faker.lorem.words({ min: 1, max: 3 })
+      );
+      const instructions = times(faker.number.int({ min: 2, max: 7 }), () =>
+        faker.lorem.sentence()
+      );
       queryRunner.query(`
             INSERT INTO "pizza" (name, rating, image_url, ingredients, instructions)
             VALUES
